30f948bc0049b888c536dff49c02dd0ce1ed3f2c
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2024-01-22 08:08:42 +0100

    Added fetching offers applied for.

--- src/routes/applications/+page.server.ts
+++ src/routes/applications/+page.server.ts
@@ -7,13 +7,12 @@ export const load: PageServerLoad = async ({ cookies }) => {
 	const token = cookies.get('applicant_token') ?? '';
 	const logged_in = token !== '';
 	if (logged_in) {
-		const response = await fetch(`${BACKEND_BASE_URL}/api/offer/all`, {
+		const response = await fetch(`${BACKEND_BASE_URL}/api/offer/get-applied-for`, {
 			method: 'GET',
 			headers: {
 				Authorization: `Bearer ${token}`
 			}
 		});
-		console.log(response.status);
 		const text = await response.text();
 		const data = parse_offer_list(text);
 		return { data };
--- src/routes/applications/+page.svelte
+++ src/routes/applications/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+	import Offer from '$lib/Offer.svelte';
+
 	export let data;
 </script>
 
 {#if data.logged_in}
-	<h1>Your applications</h1>
-	<div>
-		<!-- TODO: offers -->
-	</div>
+	{#if data.data.length !== 0}
+		<h1>Your applications</h1>
+		<div>
+			{#each data.data as offer (offer.id)}
+				<Offer {offer} />
+			{/each}
+		</div>
+	{:else}
+		<h1>No applications found.</h1>
+	{/if}
 {:else}
 	<h1>Log in to see your applications.</h1>
 {/if}
