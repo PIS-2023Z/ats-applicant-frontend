9676bf8306be7da0fde326c365c6132ce4f73b4f
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2024-01-23 12:23:13 +0100

    Added offer searching.

--- src/routes/offers/+page.server.ts
+++ src/routes/offers/+page.server.ts
@@ -1,23 +1,52 @@
 import { BACKEND_BASE_URL } from '$env/static/private';
 import { parse_offer_list } from '$lib';
 import type { ApiOfferResponse } from '$lib/types';
+import { redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ cookies }) => {
+export const load: PageServerLoad = async ({ cookies, url }) => {
 	const token = cookies.get('applicant_token') ?? '';
 	const logged_in = token !== '';
 	if (logged_in) {
-		const response = await fetch(`${BACKEND_BASE_URL}/api/offer/all`, {
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${token}`
+		const query = url.searchParams.get('phrase');
+		if (query === null || query === '') {
+			const response = await fetch(`${BACKEND_BASE_URL}/api/offer/all`, {
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			console.log(response.status);
+			const text = await response.text();
+			const data = parse_offer_list(text);
+			return { data };
+		} else {
+			const response = await fetch(`${BACKEND_BASE_URL}/api/offer/get-offers-by-fraze/${query}`, {
+				method: 'GET',
+				headers: { Authorization: `Bearer ${token}` }
+			});
+			const text = await response.text();
+			if (text.length !== 0) {
+				const data = parse_offer_list(text);
+				return { data };
+			} else {
+				return { data: [] as ApiOfferResponse };
 			}
-		});
-		console.log(response.status);
-		const text = await response.text();
-		const data = parse_offer_list(text);
-		return { data };
+		}
 	} else {
 		return { data: [] as ApiOfferResponse };
+	}
+};
+export const actions: Actions = {
+	default: async ({ url, request }) => {
+		const data = await request.formData();
+		const phrase = data.get('phrase')?.toString();
+		if (phrase === undefined || phrase.trim().length === 0) {
+			url.searchParams.delete('phrase');
+			redirect(302, url);
+		} else {
+			url.searchParams.set('phrase', phrase);
+			redirect(302, url);
+		}
 	}
 };
--- src/routes/offers/+page.svelte
+++ src/routes/offers/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import Offer from '$lib/Offer.svelte';
 	export let data;
 </script>
 
 {#if data.logged_in}
 	<h1>Offers</h1>
+	<div>
+		<form method="post">
+			<label for="phrase">Filter by term:</label>
+			<input name="phrase" type="text" value={$page.url.searchParams.get('phrase') ?? ''} />
+			<button>Search</button>
+		</form>
+	</div>
 	<div>
 		{#each data.data as offer (offer.id)}
 			<div class="offer_div">
